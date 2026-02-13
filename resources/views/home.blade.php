3a55c46ce7751521cd93da782c3e86ac0368fbbe
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2026-02-13 15:40:53 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -60,33 +60,23 @@
 								<section class="card card-featured-left card-featured-primary">
 									<div class="card-body icon-container data-container" style="background-image: url('/assets/img/cheque-icon.png'); ">
 										<h3 class="amount text-dark"><strong>Post Dated Cheques</strong></h3>
-										@if(isset($pdc) && isset($pdc->Total_Balance))
-											<h2 class="amount m-0 text-primary actual-data">
-												<strong data-value="{{ $pdc->Total_Balance }}">0</strong>
-												<span class="title text-end text-dark h6"> PKR</span>
-											</h2>
-											<h2 class="amount m-0 text-primary masked-data"><strong>******</strong></h2>
-										@else
-											<h2 class="amount m-0 text-primary actual-data">
-												<strong data-value="0">0</strong>
-												<span class="title text-end text-dark h6"> PKR</span>
-											</h2>
-											<h2 class="amount m-0 text-primary masked-data"><strong>******</strong></h2>
-										@endif
+										@php
+											$totalBalance = isset($pdc) && isset($pdc->Total_Balance) ? $pdc->Total_Balance : 0;
+											$balPdc = isset($bal_pdc) && isset($bal_pdc->bal_pdc) ? $bal_pdc->bal_pdc : 0;
+										@endphp
 
-										@if(isset($bal_pdc) && isset($bal_pdc->bal_pdc))
-											<h2 class="amount m-0 text-primary actual-data">
-												<strong data-value="{{ $bal_pdc->bal_pdc }}">0</strong>
-												<span class="title text-end text-dark h6"> PKR</span>
-											</h2>
-											<h2 class="amount m-0 text-primary masked-data"><strong>******</strong></h2>
-										@else
-											<h2 class="amount m-0 text-primary actual-data">
-												<strong data-value="0">0</strong>
-												<span class="title text-end text-dark h6"> PKR</span>
-											</h2>
-											<h2 class="amount m-0 text-primary masked-data"><strong>******</strong></h2>
-										@endif
+										<h2 class="amount m-0 text-primary actual-data">
+											<strong data-value="{{ $totalBalance }}">{{ $totalBalance }}</strong>
+											<span class="title text-end text-dark h6"> PKR</span>
+											/
+											<strong data-value="{{ $balPdc }}">{{ $balPdc }}</strong>
+											<span class="title text-end text-dark h6"> PKR</span>
+										</h2>
+
+										<h2 class="amount m-0 text-primary masked-data">
+											<strong>****** / ******</strong>
+										</h2>
+
 
 										<div class="summary-footer">
 											<a class="text-primary text-uppercase" href="#">View Details</a>
