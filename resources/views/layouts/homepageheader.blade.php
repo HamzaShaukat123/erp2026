fe30f2e7f87e3254d05b6f21a70d652f7b2fac01
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2026-06-08 17:04:09 +0500

    Update homepageheader.blade.php

--- resources/views/layouts/homepageheader.blade.php
+++ resources/views/layouts/homepageheader.blade.php
@@ -106,7 +106,7 @@
 				<div class="dropdown-menu" >
 					<ul class="list-unstyled">
 						<li>
-							<a role="menuitem" tabindex="-1" href="#changePassword" class="mb-1 mt-1 me-1 modal-with-zoom-anim ws-normal"><i class="bx bx-lock"></i> Change Password</a>
+							<a role="menuitem" tabindex="-1" href="#changePassword" class="mb-1 mt-1 me-1 modal-with-zoom-anim ws-normal"><i class="bx bx-lock"></i> Chang25e Password</a>
 						</li>
 						<li>	
 							<form action="/logout" method="POST">
