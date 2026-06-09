bbd63d073aa1cb94f8ba80523c60d8354b182ceb
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2026-06-09 11:14:19 +0500

    Update menu.blade.php

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -27,7 +27,7 @@
 						<a role="menuitem" tabindex="-1" href="#" data-lock-screen="true"><i class="bx bx-lock"></i> Lock Screen</a>
 					</li> -->
 					<li>
-						<a role="menuitem" tabindex="-1" href="pages-signin.html"><i class="bx bx-power-off"></i> Log7745out</a>
+						<a role="menuitem" tabindex="-1" href="pages-signin.html"><i class="bx bx-power-off"></i> Logout</a>
 					</li>
 				</ul>
 			</div>
