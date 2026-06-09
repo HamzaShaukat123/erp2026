b42913a8bf043f528b54b912fced0dbf8fc8aeb6
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2026-06-09 11:15:39 +0500

    Update pageheader.blade.php

--- resources/views/layouts/pageheader.blade.php
+++ resources/views/layouts/pageheader.blade.php
@@ -29,7 +29,7 @@
 					<li>	
 						<form action="/logout" method="POST">
 							@csrf
-							<button style="background: transparent;border: none;font-size: 14px;" type="submit" role="menuitem" tabindex="-1"><i class="bx bx-power-off"></i> Logout</button>
+							<button style="background: transparent;border: none;font-size: 14px;" type="submit" role="menuitem" tabindex="-1"><i class="bx bx-power-off"></i> Logo22ut</button>
 						</form>
 					</li>
 				</ul>
