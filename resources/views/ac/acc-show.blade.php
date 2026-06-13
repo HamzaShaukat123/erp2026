cc9a2735077d78edabea4963d17411d445452d12
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2026-06-13 15:42:50 +0500

    Update acc-show.blade.php

--- resources/views/ac/acc-show.blade.php
+++ resources/views/ac/acc-show.blade.php
@@ -40,12 +40,6 @@
 
     </div>
 
-    <!-- <div class="mt-2 text-end">
-        <button onclick="window.print()" class="btn btn-sm btn-primary">
-            Print Report
-        </button>
-    </div> -->
-
 
 	<div class="text-end">
 		<button onclick="window.print()" class="btn btn-danger mt-2 mb-2" target="_blank">
@@ -140,6 +134,12 @@
 
 </table>
 
+</div>
+
+<div class="text-end">
+	<button onclick="window.print()" class="btn btn-danger mt-2 mb-2" target="_blank">
+		<i class="fas fa-print"></i> Print
+	</button>
 </div>
 
 </div>
