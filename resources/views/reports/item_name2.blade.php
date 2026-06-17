9cde9b997300e607ac97f2b86d2b5a2691c79438
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2026-06-17 16:23:49 +0500

    Update item_name2.blade.php

--- resources/views/reports/item_name2.blade.php
+++ resources/views/reports/item_name2.blade.php
@@ -120,7 +120,8 @@
                                         <a class="mb-1 mt-1 me-1 btn btn-success" aria-label="Export to Excel" onclick="downloadExcel('purchase')"><i class="fa fa-file-excel"></i> Excel</a>   
                                     </div>
                                     
-                                    <div class="col-12 mt-4">
+                                    <!-- <div class="col-12 mt-4"> -->
+                                        <div class="modal-wrapper table-scroll">
                                         <table class="table table-bordered table-striped mb-0">
                                             <thead>
                                                 <tr>
