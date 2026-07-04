d6b9f59e04a38daa986734473a3dddd34b9f7c75
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2026-07-04 17:02:01 +0500

    Update DashboardPettyCashTabController.php

--- app/Http/Controllers/DashboardPettyCashTabController.php
+++ app/Http/Controllers/DashboardPettyCashTabController.php
@@ -39,8 +39,8 @@ class DashboardPettyCashTabController extends Controller
         $jv1 = new petty_cash();
         $jv1->created_by = session('user_id');
 
-        if ($request->has('user_id') && $request->user_id) {
-            $jv1->user_id_hidden=$request->user_id_hidden;
+        if ($request->has('user_id_hidden') && $request->user_id_hidden) {
+            $jv1->user_id=$request->user_id_hidden;
         }
         if ($request->has('date') && $request->date) {
             $jv1->date=$request->date;
