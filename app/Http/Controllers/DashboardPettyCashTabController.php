8233043fe2da1bd5eacc6af08cf817095615bf12
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2026-07-06 15:40:08 +0500

    Update DashboardPettyCashTabController.php

--- app/Http/Controllers/DashboardPettyCashTabController.php
+++ app/Http/Controllers/DashboardPettyCashTabController.php
@@ -60,7 +60,7 @@ class DashboardPettyCashTabController extends Controller
 
         
         // return redirect()->route('home');
-        return redirect('/home');
+        return redirect('/home')->with('success', 'Petty Cash added successfully!');
     }
 
 
@@ -99,7 +99,7 @@ class DashboardPettyCashTabController extends Controller
             'updated_by' => session('user_id'),
         ]);
 
-        return redirect('/home');
+        return redirect('/home')->with('success', 'Petty Cash updated successfully!');
     }
 
 
