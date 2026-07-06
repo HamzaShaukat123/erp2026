da6f0058afa9b0fc9b78eaaef9fafcf26c973248
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2026-07-06 15:35:03 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2236,7 +2236,7 @@
 									@endforeach
 								</select>   
                                   
-								<input type="text" name="user_id_hidden" id="user_id_hidden_update">                       
+								<input type="hidden" name="user_id_hidden" id="user_id_hidden_update">                       
                             </div>
                             <div class="col-lg-6 mb-2">
                                 <label>Add<span style="color: red;"><strong>*</strong></span></label>
